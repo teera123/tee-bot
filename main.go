c3fcbe681a3e3b5e147d44e9b2880dd1bb895e30
Teerapat <teera123@me.com>
2017-09-08 23:20:06 +0700

    add log

--- main.go
+++ main.go
@@ -101,12 +101,14 @@ func lineTextResponse(msg string, source *linebot.EventSource) *linebot.TextMess
 		conn.Send("MULTI")
 		conn.Send("HMSET", hkey, "interval", t, "last_push", time.Now())
 		conn.Send("SADD", skey, hkey)
-		reply, err := redis.String(conn.Do("EXEC"))
+		reply, err := conn.Do("EXEC")
 		if err != nil {
 			rtn = "redis พังอ่ะ " + err.Error()
 			goto ex
 		}
-		rtn = "ตั้งค่าเรียบร้อยคร๊าบบบบบ DED " + reply
+		fmt.Println("reply:", reply, skey, hkey)
+
+		rtn = "ตั้งค่าเรียบร้อยคร๊าบบบบบ DED"
 	case strings.HasPrefix(command, "viewinterval"):
 		conn := rdPool.Get()
 		defer conn.Close()
