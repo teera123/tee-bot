4270d6f6794aaa19e79eb41a7047d184b5d2240a
Teerapat <teera123@me.com>
2017-09-08 13:45:24 +0700

    first test with line bot sdk

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
-//import "github.com/line/line-bot-sdk-go/linebot"
+var bot *linebot.Client
 
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		panic("$PORT must be set")
+	}
+
+	var err error
+	bot, err = linebot.New(os.Getenv("CHANNEL_SECRET"), os.Getenv("CHANNEL_TOKEN"))
+	if err != nil {
+		panic("cannot init line bot")
 	}
 
 	r := gin.New()
@@ -23,5 +31,24 @@ func main() {
 }
 
 func botHandler(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"status": "success"})
+	events, err := bot.ParseRequest(c.Request)
+	if err != nil {
+		log.Println("line bot parse request error:", err)
+
+		if err == linebot.ErrInvalidSignature {
+			c.Writer.WriteHeader(http.StatusBadRequest)
+		} else {
+			c.Writer.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	for _, event := range events {
+		switch event.Type {
+		case linebot.EventTypeMessage:
+			if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("test from heroku")).Do(); err != nil {
+				log.Println("reply message error:", err)
+			}
+		}
+	}
 }
